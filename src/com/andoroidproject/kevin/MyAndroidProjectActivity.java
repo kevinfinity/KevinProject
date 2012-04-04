f9d5049f5122ae772be96facda8afd19f69a0766
Kevin Kim <kevykevz.emailaddress@gmail.com>
2012-04-04 13:03:51 +0300

    all my xml pages

---
+++ src/com/andoroidproject/kevin/MyAndroidProjectActivity.java
@@ -0,0 +1,13 @@
+package com.andoroidproject.kevin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyAndroidProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
